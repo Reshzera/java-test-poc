f5076f919ff1eaeb5f4768149b5583f4e499f3f6
Reshzera <reoshiro13@gmail.com>
2025-06-30 17:59:45 -0400

    feat: add application porperties

--- src/test/java/com/example/java_test_poc/JavaTestPocApplicationTests.java
+++ src/test/java/com/example/java_test_poc/JavaTestPocApplicationTests.java
@@ -3,9 +3,12 @@ package com.example.java_test_poc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class JavaTestPocApplicationTests {
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 }
